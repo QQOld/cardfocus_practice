554ac2f8a158db43e80669380fd75d8a895f2406
artem.bataev <artem.bataev2016@yandex.ru>
2022-03-31 05:32:49 +0300

    Указательный палец вместо кнопок, перекинул пару методов в класс

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,56 @@
 // 1 4 7 10 13 16 19
 // 2 5 8 11 14 17 20
 // 3 6 9 12 15 18 21
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 List<Image> images =
     List.generate(22, (index) => Image.asset("assets/img/${index + 1}.png"));
 
-/*class ImgCol extends StatefulWidget {
-  const ImgCol(
-      {required this.number,
-      required this.animation,
-      required this.controller,
-      Key? key})
-      : super(key: key);
-  final int number;
-  final Animation<double> animation;
-  final AnimationController controller;
+class ShuffleCalc {
+  static List<Image> createHelpList() {
+    List<Image> helpList = List.generate(22, (index) => images[index]);
+    for (int i = 0; i <= 2; i++) {
+      for (int j = i * 7 + 1, k = 1 + i; j <= 7 + i * 7; j++, k += 3) {
+        helpList[j] = images[k];
+      }
+    }
+    return helpList;
+  }
 
-  @override
-  _ImgColState createState() => _ImgColState();
-}
-
-class _ImgColState extends State<ImgCol> with SingleTickerProviderStateMixin {
-  @override
-  void initState() {
-    super.initState();
-
-    widget.animation
-      ..addListener(() {
-        setState(() {});
-      })
-      ..addStatusListener((status) {
-        if (status == AnimationStatus.completed) {
-        } else if (status == AnimationStatus.dismissed) {
-          widget.controller.forward();
+  static void shuffleAfterChoice(int column) {
+    List<Image> helpList = createHelpList();
+    Image a;
+    switch (column) {
+      case 1:
+        for (int i = 1; i <= 7; i++) {
+          a = helpList[i];
+          helpList[i] = helpList[i + 7];
+          helpList[i + 7] = a;
         }
-      });
-    widget.controller.forward();
+        for (int i = 1; i <= 21; i++) {
+          images[i] = helpList[i];
+        }
+        break;
+      case 2:
+        for (int i = 1; i <= 21; i++) {
+          images[i] = helpList[i];
+        }
+        break;
+      case 3:
+        for (int i = 8; i <= 14; i++) {
+          a = helpList[i];
+          helpList[i] = helpList[i + 7];
+          helpList[i + 7] = a;
+        }
+        for (int i = 1; i <= 21; i++) {
+          images[i] = helpList[i];
+        }
+        break;
+    }
   }
-
-  @override
-  void dispose() {
-    widget.controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: List.generate(
-          7,
-          (index) => Positioned(
-                top: index * widget.animation.value,
-                child: ConstrainedBox(
-                  constraints: const BoxConstraints(
-                    minWidth: 80,
-                    maxWidth: 130,
-                  ),
-                  child: images[widget.number + index * 3],
-                ),
-              )),
-    );
-  }
-}*/
+}
 
 class CardsDisplay extends StatefulWidget {
   const CardsDisplay({Key? key}) : super(key: key);
@@ -81,6 +69,7 @@ class _CardsDisplayState extends State<CardsDisplay>
 
   bool mainAnimIsCompleted = false;
   bool isCardChosen = true;
+  bool isShuffling = false;
   int columnChoice = 0;
   int _choiceCount = 0;
 
@@ -100,9 +89,26 @@ class _CardsDisplayState extends State<CardsDisplay>
       })
       ..addStatusListener((status) {
         if (status == AnimationStatus.dismissed) {
-          controller.forward();
+          isShuffling = true;
+          Future.delayed(const Duration(milliseconds: 1700), () {
+            setState(() {
+              isShuffling = false;
+            });
+            Future.delayed(const Duration(milliseconds: 1700), () {
+              controller.forward();
+            });
+          });
         }
       });
+  }
+
+  double calcCardSize(BuildContext context) {
+    return MediaQuery.of(context).size.height < 520 ||
+            MediaQuery.of(context).size.width < 520
+        ? min(MediaQuery.of(context).size.width,
+                MediaQuery.of(context).size.height) /
+            4
+        : 130;
   }
 
   void _changeToVertical() {
@@ -121,7 +127,9 @@ class _CardsDisplayState extends State<CardsDisplay>
       setState(() {
         _choiceCount++;
         columnChoice = num;
-        Image a;
+
+        ShuffleCalc.shuffleAfterChoice(columnChoice);
+        /*Image a;
         List<Image> helpList = List.generate(22, (index) => images[index]);
 
         for (int i = 0; i <= 2; i++) {
@@ -156,7 +164,7 @@ class _CardsDisplayState extends State<CardsDisplay>
               images[i] = helpList[i];
             }
             break;
-        }
+        }*/
       });
     });
   }
@@ -198,52 +206,41 @@ class _CardsDisplayState extends State<CardsDisplay>
     } else if (isCardChosen) {
       return Scaffold(
         backgroundColor: Colors.transparent,
-        body: TweenAnimationBuilder(
-          tween: Tween<double>(begin: 0.0, end: 1.0),
-          duration: const Duration(milliseconds: 2500),
-          curve: Curves.easeInCubic,
-          builder: (_, double move, __) {
-            return Stack(
-                clipBehavior: Clip.none,
-                children: [
-              ...List.generate(
-                  11,
-                  (index) => Positioned(
-                        width: 130,
-                        top: 0,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: TweenAnimationBuilder(
+            tween: Tween<double>(begin: 0.0, end: 1.0),
+            duration: const Duration(milliseconds: 2500),
+            curve: Curves.easeInCubic,
+            builder: (_, double move, __) {
+              return Stack(clipBehavior: Clip.none, children: [
+                ...List.generate(
+                    11,
+                    (index) => Positioned(
+                          width: calcCardSize(context),
+                          top: 0,
+                          left: move *
+                              index *
+                              (MediaQuery.of(context).size.width - 130) *
+                              (1 / 11),
+                          child: images[index],
+                        )),
+                ...List.generate(
+                    11,
+                    (index) => Positioned(
+                        width: calcCardSize(context),
+                        top: MediaQuery.of(context).size.height > 400
+                            ? 180
+                            : MediaQuery.of(context).size.height - 220,
                         left: move *
                             index *
-                            MediaQuery.of(context).size.width *
-                            (1/11),
-                        child: images[index],
-                      )),
-              ...List.generate(
-                  11,
-                  (index) => Positioned(
-                        width: 130,
-                        top: 180,
-                        left: move *
-                            index *
-                            MediaQuery.of(context).size.width *
-                            (1/11),
+                            (MediaQuery.of(context).size.width - 130) *
+                            (1 / 11),
                         child: images[index + 11]))
-            ]
-                /*List.generate(
-                    21,
-                        (index) => Positioned(
-                        top: index < 10 ? 0 : 180,
-                        left: index < 10 ? move * index : move * index - move * 10,
-                        child: ConstrainedBox(
-                          constraints: const BoxConstraints(
-                            minWidth: 80,
-                            maxWidth: 130,
-                          ),
-                          child: images[index + 1],
-                        )))*/
-
-                );
-          },
-          onEnd: () => setState(() => mainAnimIsCompleted = true),
+              ]);
+            },
+            onEnd: () => setState(() => mainAnimIsCompleted = true),
+          ),
         ),
         floatingActionButton: ElevatedButton(
           onPressed: mainAnimIsCompleted ? _changeToVertical : null,
@@ -252,121 +249,66 @@ class _CardsDisplayState extends State<CardsDisplay>
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       );
     } else {
-      return Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          textDirection: TextDirection.ltr,
-          children: List.generate(
-            3,
-            (num) => SizedBox(
-              width: 130,
-              child: Column(
-                children: [
-                  Expanded(
+      return Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+              clipBehavior: Clip.none,
+              textDirection: TextDirection.ltr,
+              children: List.generate(
+                3,
+                (num) => AnimatedPositioned(
+                  duration: const Duration(milliseconds: 1500),
+                  curve: Curves.easeOutSine,
+                  left: isShuffling
+                      ? MediaQuery.of(context).size.width / 2 -
+                          calcCardSize(context) / 2
+                      : MediaQuery.of(context).size.width / 4 * (num + 1) -
+                          calcCardSize(context) / 2,
+                  width: calcCardSize(context),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
                     child: Stack(
-                      clipBehavior: Clip.none,
+                        alignment: Alignment.center,
+                        clipBehavior: Clip.none,
                         children: [
-                      ...List.generate(
-                          7,
-                          (index) => Positioned(
-                                width: 130,
-                                top: index *
-                                    animation.value *
-                                    MediaQuery.of(context).size.height *
-                                    1/12,
-                                // индекс (0..2) + 1 = номер колонки, умножение на 3 для правильного разложения карт(в каждый столбец по порядку, т.е. 1 столбец - 1, 4, 7...)
-                                child: images[num + 1 + index * 3],
-                              )),
-                      Positioned(
-                          width: 130,
-                          top: -shirtAnimation.value * 205,
-                          child: images[0])
-                    ]),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(vertical: 20),
-                    child: ElevatedButton(
-                        onPressed: () => _chooseColumn(num + 1),
-                        child: const Text("Выбрать")),
-                  )
-                ],
-              ),
-            ),
-          )
-          /*[
-          SizedBox(
-            width: 130,
-            child: Column(
-              children: [
-                Expanded(
-                  child: Stack(
-                    children: List.generate(
-                        7,
-                        (index) => Positioned(
-                              width: 130,
-                              top: index * animation.value * MediaQuery.of(context).size.height/7 * 0.01,
-                              child: images[1 + index * 3],
-                            )),
+                          ...List.generate(
+                              7,
+                              (index) => Positioned(
+                                    width: calcCardSize(context),
+                                    top: index *
+                                        animation.value *
+                                        MediaQuery.of(context).size.height *
+                                        1 /
+                                        12,
+                                    // индекс (0..2) + 1 = номер колонки, умножение на 3 для правильного разложения карт(в каждый столбец по порядку, т.е. 1 столбец - 1, 4, 7...)
+                                    child: images[num + 1 + index * 3],
+                                  )),
+                          Positioned(
+                              width: calcCardSize(context),
+                              top: -shirtAnimation.value * 205,
+                              child: images[0]),
+                          Positioned(
+                            width: 100,
+                            bottom: 50,
+                            child: IconButton(
+                              onPressed: () => _chooseColumn(num + 1),
+                              icon: const Image(
+                                  image: AssetImage("assets/finger.png")),
+                              iconSize: 100,
+                              color: Colors.transparent,
+                              hoverColor: Colors.transparent,
+                            ),
+                          ),
+                        ]),
                   ),
                 ),
-                Container(
-                  padding: const EdgeInsets.symmetric(vertical: 20),
-                  child: ElevatedButton(
-                      onPressed: () => _chooseColumn(1),
-                      child: const Text("Выбрать")),
-                )
-              ],
-            ),
-          ),
-          SizedBox(
-            width: 130,
-            child: Column(
-              children: [
-                Expanded(
-                  child: Stack(
-                    children: List.generate(
-                        7,
-                        (index) => Positioned(
-                              width: 130,
-                              top: index * animation.value * MediaQuery.of(context).size.height/7 * 0.01,
-                              child: images[2 + index * 3],
-                            )),
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(vertical: 20),
-                  child: ElevatedButton(
-                      onPressed: () => _chooseColumn(2),
-                      child: const Text("Выбрать")),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            width: 130,
-            child: Column(
-              children: [
-                Expanded(
-                  child: Stack(
-                    children: List.generate(
-                        7,
-                        (index) => Positioned(
-                              width: 130,
-                              top: index * animation.value * MediaQuery.of(context).size.height/7 * 0.01,
-                              child: images[3 + index * 3],
-                            )),
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(vertical: 20),
-                  child: ElevatedButton(
-                      onPressed: () => _chooseColumn(3),
-                      child: const Text("Выбрать")),
-                ),
-              ],
-            ),
-          ),
-        ],*/
-          );
+              )),
+        ),
+      );
     }
   }
 
@@ -385,8 +327,7 @@ void main() {
           backgroundColor: Color.fromRGBO(69, 152, 66, 0.9),
           body: Center(
             child: Padding(
-              padding: EdgeInsets.all(20),
-                child: CardsDisplay()
-            ),
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: CardsDisplay()),
           ))));
 }
